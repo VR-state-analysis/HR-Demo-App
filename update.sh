2afed39d6ee6f82cece3b501ae7545c3268d0ac9
Ken Shibata <ken.shibata+git@kiyuri.ca>
2025-10-18 16:38:57 -0400

    add setcap call to update script

--- update.sh
+++ update.sh
@@ -6,4 +6,5 @@ git pull
 go build -o hr-demo-app-server ./cmd/server/main.go
 sudo systemctl stop hr-demo-app-server
 sudo cp hr-demo-app-server /usr/local/bin
+sudo setcap 'cap_net_bind_service=+ep' /usr/local/bin/hr-demo-app-server
 sudo systemctl start hr-demo-app-server
